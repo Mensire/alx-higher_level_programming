6c08593d4f7c2e7d9a06a9df5786b83e4aa3fa0c
Ruth <mensirer@gmail.com>
2022-08-26 15:30:10 -0400

    hexadecimal combined with characters

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for decimal in range(0, 100):
     if decimal == 99:
         print("{}".format(decimal), end='\n')
     else:
-        print("{:02}".format(decimal), end=',')
+        print("{:02}".format(decimal), end=', ')
