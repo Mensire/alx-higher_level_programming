1282efcf90072cb37317b75b879469c18dfb34ab
Ruth <mensirer@gmail.com>
2022-09-01 10:24:50 -0400

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
 
 if __name__ == '__main__':
-    def vdir(hidden_4):
-        if name[:2] != "__":
-            print(name)
+    import hidden_4
+    names = dir(hidden_4)
+    for i in names:
+        if i[:2] != "__":
+            print(i)
