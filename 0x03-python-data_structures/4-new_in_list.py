529e01c06b154f3f3c6b18468ae4c7edf13d7573
Ruth <mensirer@gmail.com>
2022-09-02 18:48:48 -0400

    replace an element and keep old copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    my_copy = my_list.copy()
-    if idx < 0:
-        return my_copy
-    elif idx > (len(my_list) - 1):
-        return my_copy
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
     else:
-        my_copy[idx] = element
-        return 
+        my_copy = my_list.copy()
+        my_list[idx] = element
+        return my_list
