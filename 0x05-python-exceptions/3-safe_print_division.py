f1ed29d0fc8990cdb83e926be9be57f2580195d5
Ruth <mensirer@gmail.com>
2022-09-14 06:29:17 -0400

    divides two integers and prints the result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,6 +3,8 @@ def safe_print_division(a, b):
     try:
         result = a/b
     except (ZeroDivisionError, TypeError):
+        result = a/b
         return None
     finally:
         print("Inside result: {}".format(result))
+        return result
