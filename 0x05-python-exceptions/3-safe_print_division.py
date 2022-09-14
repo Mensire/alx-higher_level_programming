0290bdc63a2c1feeafdcfad39f107d5e6b78b8af
Ruth <mensirer@gmail.com>
2022-09-14 07:57:28 -0400

    divide integers

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        c = 0.0
-        c = a / b
-        return c
-    except (ZeroDivisionError, TypeError):
-        print("Inside result: {}".format(None))
-        return None
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
     finally:
-        if c != 0.0:
-            print("Inside result: {}".format(c))
+        print("Inside result: {}".format(div))
+    return (div)
