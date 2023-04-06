6428ed7963855f7e7005957a4f9684e34d256cdc
Mensire <mensirer@gmail.com>
2023-04-06 11:05:09 +0300

    empty class square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+
+
+"""docstring for square"""
+
+
 class Square:
-
-
-    """docstring for the class square"""
-
-
+    """docstring for init"""
     def __init__(self):
-        """docstring for the method"""
         pass
